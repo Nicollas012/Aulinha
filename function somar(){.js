0e5e00e204e86ffd489a251f1291453f0e0a34ab
Nicollas012 <101215133+Nicollas012@users.noreply.github.com>
2022-12-04 00:36:01 -0300

    Create function somar(){.js

---
+++ function somar(){.js
@@ -0,0 +1,9 @@
+function somar(){
+    var num1 = document.getElementById("n1").value;
+    var num2 = document.getElementById("n2").value;
+    var num3 = document.getElementById("n3").value;
+    var res = document.getElementById("resultado");
+
+    res.innerHTML = parseInt(num1) + parseInt(num2) + parseInt(num3);
+}
+Footer
